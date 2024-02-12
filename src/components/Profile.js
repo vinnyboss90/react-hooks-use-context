3a753f72b569bb2c511c85682e331bd1f2fcc6e7
vinnyegon90 <vinnyegon90@gmail.com>
2024-02-12 13:05:12 +0300

    Done

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,15 @@
-import React from "react";
+// import the useContext hook
+import React, { useContext } from "react";
+// import the UserContext we created
+import { UserContext } from "../context/user";
 import Interests from "./Interests";
 
-function Profile({ user, theme }) {
+function Profile({ theme }) {
+  // call useContext with our UserContext
+  const { user } = useContext(UserContext);
+
+  // now, we can use the user object just like we would if it was passed as a prop!
+  console.log(user);
   if (!user) return <h2>Please Login To View Profile</h2>;
   return (
     <div>
